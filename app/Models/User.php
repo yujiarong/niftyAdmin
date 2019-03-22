769bbd1fbfa02af8b09bf72ef353f435b6e2c9b5
yujiarong <yujiarong@sailvan.com>
2019-03-22 10:51:29 +0800

    Passport

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,12 +6,14 @@ use Illuminate\Notifications\Notifiable;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Spatie\Permission\Traits\HasRoles;
 use App\Models\Traits\ActionLogger;
+use Laravel\Passport\HasApiTokens;
 
 class User extends Authenticatable
 {
     use Notifiable;
     use HasRoles;
     use ActionLogger;
+    use HasApiTokens;
     /**
      * The attributes that are mass assignable.
      *

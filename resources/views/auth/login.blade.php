07663ed4580d8ddb3037cc08c297b3245fe63042
yujiarong <yujiarong@sailvan.com>
2019-03-22 11:44:29 +0800

    模板  默认账号密码

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -67,11 +67,11 @@
                     <form action="{{ route('login') }}" method="post">
                         {{ csrf_field() }}
                         <div class="form-group has-feedback">
-                            <input type="text" class="form-control"  name="email" placeholder="Email" autofocus>
+                            <input type="text" class="form-control"  name="email" placeholder="Email" autofocus value="xiaoyan@sailvan.com">>
                             <span class="glyphicon glyphicon-envelope form-control-feedback"></span>
                         </div>
                         <div class="form-group has-feedback">
-                            <input type="password" class="form-control"  name="password" placeholder="Password">
+                            <input type="password" class="form-control"  name="password" placeholder="Password" value="123456">
                             <span class="glyphicon glyphicon-lock form-control-feedback"></span>
                         </div>
                         <div class="checkbox pad-btm text-left">
